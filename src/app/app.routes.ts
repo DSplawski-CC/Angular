33b3127055ac028e9445d0dac4d312473aa8c063
Daniel Splawski <daniel.splawski@prores.no>
2025-02-11 08:24:06 +0100

    Prepared routing for page not found

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,11 +2,6 @@ import { Routes } from '@angular/router';
 
 
 export const routes: Routes = [
-  // {
-  //   path: '',
-  //   redirectTo: '/movies',
-  //   pathMatch: 'full',
-  // },
   {
     loadComponent: () => import('app/movies/movies-list/movies-list.component').then(component => component.MoviesListComponent),
     title: 'Movies',
@@ -15,6 +10,15 @@ export const routes: Routes = [
   {
     loadComponent: () => import('app/movies/movie/movie.component').then(component => component.MovieComponent),
     title: 'Movie',
-    path: 'movie/:id',
+    path: 'movie/:movieId',
   },
+  {
+    loadComponent: () => import('@shared/components/page-not-found/page-not-found.component').then(component => component.PageNotFoundComponent),
+    title: 'Page not found',
+    path: 'page-not-found',
+  },
+  {
+    path: '**',
+    redirectTo: 'page-not-found',
+  }
 ];
