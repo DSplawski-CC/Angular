874039a328e9f5132685fcb34fd79ee4007076e4
Daniel Splawski <daniel.splawski@prores.no>
2025-02-18 14:24:44 +0100

    Reimplemented reviewForm formGroup based on FormBuilder

--- src/app/movies/add-review/add-review.component.ts
+++ src/app/movies/add-review/add-review.component.ts
@@ -1,11 +1,11 @@
 import { Component, EventEmitter, Output } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ReviewData } from '@/movies/types';
 import { FormInputComponent } from '@shared/components/form-input/form-input.component';
 
 
 type DataForm<T extends Object> = {
-  [Key in keyof T]: FormControl<T[Key] | null>
+  [Key in keyof T]: FormControl<T[Key]>
 }
 
 @Component({
@@ -17,12 +17,16 @@ export class AddReviewComponent {
 
   @Output('onSubmit') onSubmitEvent = new EventEmitter<ReviewData>();
 
-  reviewForm = new FormGroup({
-    title: new FormControl('', { nonNullable: false, validators: [Validators.required, Validators.minLength(3), Validators.maxLength(20)] }),
-    content: new FormControl('', { nonNullable: false, validators: [Validators.required, Validators.minLength(3), Validators.maxLength(1000)] }),
-    author: new FormControl('', { nonNullable: false, validators: [Validators.required, Validators.minLength(3), Validators.maxLength(10) ]}),
-    rating: new FormControl(null, { nonNullable: true, validators: [Validators.required, Validators.min(1), Validators.max(10)] }),
-  } satisfies DataForm<ReviewData>);
+  reviewForm: FormGroup<DataForm<ReviewData>>
+  constructor() {
+    const formBuilder = new FormBuilder().nonNullable;
+    this.reviewForm = formBuilder.group({
+      title: formBuilder.control('', { validators: [Validators.required, Validators.minLength(3), Validators.maxLength(20)] }),
+      content: formBuilder.control('', { validators: [Validators.required, Validators.minLength(3), Validators.maxLength(1000)] }),
+      author: formBuilder.control('', { validators: [Validators.required, Validators.minLength(3), Validators.maxLength(10)] }),
+      rating: formBuilder.control(NaN, { validators: [Validators.required, Validators.min(1), Validators.max(10)] }),
+    });
+  }
 
   onSubmit() {
     this.onSubmitEvent.emit(this.reviewForm.value as unknown as ReviewData);
