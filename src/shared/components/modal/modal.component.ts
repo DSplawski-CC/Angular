68c04e21605fd9705f9e99825b5591f12341cedf
Daniel Splawski <daniel.splawski@prores.no>
2025-02-20 14:13:24 +0100

    Implemented modal with closing on escape, click outside and blocking wheeling outside

---
+++ src/shared/components/modal/modal.component.ts
@@ -0,0 +1,48 @@
+import {
+  Component,
+  contentChild,
+  effect,
+  HostListener,
+  input,
+  model,
+  TemplateRef,
+} from '@angular/core';
+import { NgTemplateOutlet } from '@angular/common';
+import { ClickOutsideDirective } from '@shared/directives/click-outside.directive';
+import { BlockWheelingDirective } from '@shared/directives/block-wheeling.directive';
+
+
+@Component({
+  selector: 'app-modal',
+  imports: [
+    NgTemplateOutlet,
+    ClickOutsideDirective,
+    BlockWheelingDirective,
+  ],
+  templateUrl: './modal.component.html',
+  host: {
+    class: 'fixed top-auto bottom-auto right-auto left-auto',
+  }
+})
+export class ModalComponent {
+  customActions = contentChild<TemplateRef<any>>('customActions');
+  show = model(false);
+  activator = input<HTMLElement>();
+
+  constructor() {
+    effect(() => {
+      const activator = this.activator();
+      if (activator) {
+        activator.onclick = () => { console.log('activator click'); this.show.set(true) };
+      }
+    })
+  }
+
+  @HostListener('window:keydown.esc', ['$event'])
+  closeModal() {
+    if (this.show()) {
+      console.log('closeModal')
+      this.show.set(false);
+    }
+  }
+}
