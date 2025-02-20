dbfc1714c4b9f41dc477b8a3e16dda91f8d88464
Daniel Splawski <daniel.splawski@prores.no>
2025-02-20 14:12:03 +0100

    Fixed not provided DecimalPipe

--- src/app/movies/movies-list/movies-list.component.ts
+++ src/app/movies/movies-list/movies-list.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { MoviesApiService } from '@/movies/movies-api.service';
-import { DatePipe } from '@angular/common';
+import { DatePipe, DecimalPipe } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { RatingFormatPipe } from "@shared/pipes/rating-format.pipe";
 
@@ -8,6 +8,7 @@ import { RatingFormatPipe } from "@shared/pipes/rating-format.pipe";
 @Component({
   selector: 'app-movies-list',
   imports: [DatePipe, RouterLink, RatingFormatPipe],
+  providers: [DecimalPipe],
   templateUrl: './movies-list.component.html',
 })
 export class MoviesListComponent {
