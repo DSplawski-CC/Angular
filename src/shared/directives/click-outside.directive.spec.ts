99130e4574558501b4c2419dd372d569ee563209
Daniel Splawski <daniel.splawski@prores.no>
2025-02-25 15:42:00 +0100

    Prepared testbed for click-outside.directive

--- src/shared/directives/click-outside.directive.spec.ts
+++ src/shared/directives/click-outside.directive.spec.ts
@@ -1,8 +1,33 @@
 import { ClickOutsideDirective } from './click-outside.directive';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component, DebugElement, signal } from '@angular/core';
+import { By } from '@angular/platform-browser';
+
+@Component({
+  template: `<div [appClickOutside]></div>`,
+  imports: [ClickOutsideDirective],
+})
+class TestComponent {
+  readonly ignore = signal(true);
+}
 
 describe('ClickOutsideDirective', () => {
-  it('should create an instance', () => {
-    const directive = new ClickOutsideDirective();
-    expect(directive).toBeTruthy();
+  let fixture: ComponentFixture<TestComponent>;
+  let directiveElement: DebugElement;
+  let appliedDirective: ClickOutsideDirective;
+
+  beforeEach(() => {
+    fixture = TestBed.configureTestingModule({
+      imports: [ClickOutsideDirective, TestComponent],
+    }).createComponent(TestComponent);
+
+    fixture.detectChanges();
+
+    directiveElement = fixture.debugElement.query(By.directive(ClickOutsideDirective));
+    appliedDirective = directiveElement.injector.get(ClickOutsideDirective);
+  });
+
+  it('should be applied to element', () => {
+    expect(appliedDirective).toBeTruthy();
   });
 });
