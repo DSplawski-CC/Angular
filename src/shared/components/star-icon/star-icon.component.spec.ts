fad5ccce6f725cbcfbf348e9850db08cfb6793ac
Daniel Splawski <daniel.splawski@prores.no>
2025-02-26 15:51:44 +0100

    Handling edge cases for invalid size values Written unit test for star-icon component

--- src/shared/components/star-icon/star-icon.component.spec.ts
+++ src/shared/components/star-icon/star-icon.component.spec.ts
@@ -1,10 +1,11 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { StarIconComponent } from './star-icon.component';
+
 
 describe('StarIconComponent', () => {
   let component: StarIconComponent;
   let fixture: ComponentFixture<StarIconComponent>;
+  let svg: SVGElement
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -14,10 +15,40 @@ describe('StarIconComponent', () => {
 
     fixture = TestBed.createComponent(StarIconComponent);
     component = fixture.componentInstance;
+    svg = fixture.nativeElement.querySelector('svg');
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should have fill set to currentColor', () => {
+    expect(svg.getAttribute('fill')).toBe('currentColor');
+  });
+
+  it('should have default size 14px if not provided', () => {
+    expect(svg.style.width).toBe('14px');
+    expect(svg.style.height).toBe('14px');
+  });
+
+  it('should handle invalid sizes gracefully', () => {
+    fixture.componentRef.setInput('size', null);
+    fixture.detectChanges();
+    expect(svg.style.width).toBe('14px');
+    expect(svg.style.height).toBe('14px');
+
+    fixture.componentRef.setInput('size', -5);
+    fixture.detectChanges();
+    expect(svg.style.width).toBe('14px');
+    expect(svg.style.height).toBe('14px');
+  });
+
+  it('should set height and width', () => {
+    fixture.componentRef.setInput('size', 10);
+    fixture.detectChanges();
+
+    expect(svg.style.height).toBe('10px');
+    expect(svg.style.width).toBe('10px');
+  })
 });
--- src/shared/components/star-icon/star-icon.component.ts
+++ src/shared/components/star-icon/star-icon.component.ts
@@ -1,4 +1,4 @@
-import { Component, input } from '@angular/core';
+import { Component, computed, input } from '@angular/core';
 
 @Component({
   selector: 'app-star-icon',
@@ -6,5 +6,11 @@ import { Component, input } from '@angular/core';
   templateUrl: './star-icon.component.html',
 })
 export class StarIconComponent {
-  size = input(14);
+  private readonly defaultSize = 14;
+  size = input(this.defaultSize);
+
+  validSize = computed(() => {
+    const size = this.size();
+    return size > 0 ? size : this.defaultSize;
+  });
 }
