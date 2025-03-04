84e8b53c771dd9c44f2a1d67989df7b938dc3718
Daniel Splawski <daniel.splawski@prores.no>
2025-03-04 09:25:05 +0100

    Refactored movie-list component to make consistent interface

--- src/app/movies/movies-list/movies-list.component.ts
+++ src/app/movies/movies-list/movies-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { MoviesApiService } from '@/movies/movies-api.service';
 import { DatePipe, DecimalPipe } from '@angular/common';
 import { RouterLink } from '@angular/router';
@@ -12,13 +12,5 @@ import { RatingFormatPipe } from "@shared/pipes/rating-format.pipe";
   templateUrl: './movies-list.component.html',
 })
 export class MoviesListComponent {
-  constructor(public moviesApiService: MoviesApiService) {}
-
-  previousPage(): void {
-    this.moviesApiService.previousPage();
-  }
-
-  nextPage(): void {
-    this.moviesApiService.nextPage();
-  }
+  moviesApiService = inject(MoviesApiService);
 }
