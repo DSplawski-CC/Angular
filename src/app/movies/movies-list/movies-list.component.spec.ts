8a0bbfac11207673f8b12a198a6209644747000e
Daniel Splawski <daniel.splawski@prores.no>
2025-03-07 13:44:21 +0100

    Fixed initializing for test in movie-details.component

--- src/app/movies/movies-list/movies-list.component.spec.ts
+++ src/app/movies/movies-list/movies-list.component.spec.ts
@@ -1,33 +1,50 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed, tick } from '@angular/core/testing';
 import { MoviesListComponent } from './movies-list.component';
-import { MovieDb, MovieResponse } from 'moviedb-promise';
+import { MovieDb, MovieResponse, MovieResult, PopularMoviesRequest, PopularMoviesResponse } from 'moviedb-promise';
 import { MOVIE_DB } from '@/app.config';
+import { DebugElement } from '@angular/core';
+import { By } from '@angular/platform-browser';
 
 
 describe('MoviesListComponent', () => {
   let component: MoviesListComponent;
   let fixture: ComponentFixture<MoviesListComponent>;
-  const movie: MovieResponse = {
+  const firstMovie = {
     vote_average: 5.82,
-    vote_count: 12_503,
-    runtime: 95,
-    release_date: '2020-01-01',
     title: 'Movie 1',
     id: 1,
     overview: 'Overview 1',
-  };
+  } as unknown as MovieResult;
+  const secondMovie = {
+    vote_average: 7.521,
+    title: 'Movie 2',
+    id: 2,
+    overview: 'Overview 2',
+  } as unknown as MovieResult;
 
   beforeEach(async () => {
     const movieDb = {
-      async movieInfo(params, config): Promise<MovieResponse> {
-        if (params === '1' ||
-          (params === 1) ||
-          (typeof params === 'object' && params.id === 1)
-        ) {
-          return Promise.resolve(movie);
-        } else {
-          return Promise.reject();
+      async moviePopular(params?: PopularMoviesRequest): Promise<PopularMoviesResponse> {
+        const page = params?.page ?? 1;
+        const totalPages = 2;
+
+        if (page > totalPages) {
+          return Promise.reject(new Error('Page out of bound'));
         }
+
+        const response: PopularMoviesResponse = {
+          page: page,
+          total_pages: totalPages,
+          total_results: 2,
+          results: page === 1
+            ? [firstMovie]
+            : page === 2
+              ? [secondMovie]
+              : []
+          ,
+        }
+
+        return Promise.resolve(response);
       }
     } satisfies Partial<MovieDb>;
 
