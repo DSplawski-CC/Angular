94d99342f5e9896b1f5ea26a156f9249ca370f18
Daniel Splawski <daniel.splawski@prores.no>
2025-03-07 13:42:12 +0100

    Fixed initializing movie-details.component in tests

--- src/app/movies/movie/movie-details/movie-details.component.spec.ts
+++ src/app/movies/movie/movie-details/movie-details.component.spec.ts
@@ -2,10 +2,20 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { MovieDetailsComponent } from './movie-details.component';
 import { DecimalPipe } from '@angular/common';
+import { MovieResponse } from 'moviedb-promise';
 
 describe('MovieDetailsComponent', () => {
   let component: MovieDetailsComponent;
   let fixture: ComponentFixture<MovieDetailsComponent>;
+  const movie: MovieResponse = {
+    vote_average: 5.82,
+    vote_count: 12_503,
+    runtime: 95,
+    release_date: '2020-01-01',
+    title: 'Movie 1',
+    id: 1,
+    overview: 'Overview 1',
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -15,6 +25,7 @@ describe('MovieDetailsComponent', () => {
 
     fixture = TestBed.createComponent(MovieDetailsComponent);
     component = fixture.componentInstance;
+    fixture.componentRef.setInput('movie', movie);
     fixture.detectChanges();
   });
 
--- src/app/movies/movie/movie-details/movie-details.component.ts
+++ src/app/movies/movie/movie-details/movie-details.component.ts
@@ -1,6 +1,6 @@
 import { Component, input } from '@angular/core';
 import { StarIconComponent } from '@shared/components/star-icon/star-icon.component';
-import { DatePipe } from '@angular/common';
+import { DatePipe, DecimalPipe } from '@angular/common';
 import { MovieResponse } from 'moviedb-promise';
 import { RatingFormatPipe } from '@shared/pipes/rating-format.pipe';
 
@@ -12,8 +12,8 @@ import { RatingFormatPipe } from '@shared/pipes/rating-format.pipe';
     DatePipe,
     RatingFormatPipe,
   ],
+  providers: [DecimalPipe],
   templateUrl: './movie-details.component.html',
-  styleUrl: './movie-details.component.css',
 })
 export class MovieDetailsComponent {
   movie = input.required<MovieResponse>();
