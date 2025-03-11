64b194d3dfb13088cedd9fdc27bdd6f5c32ddb80
Daniel Splawski <daniel.splawski@prores.no>
2025-03-11 15:30:49 +0100

    Implemented interceptor for setting base url

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,6 +3,8 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { environment } from '../environments/environment';
 import { MovieDb } from 'moviedb-promise';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { apiInterceptor } from '@core/api.interceptor';
 
 
 export const API_KEY = new InjectionToken<string>('apiKey');
@@ -13,6 +15,7 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    provideHttpClient(withInterceptors([apiInterceptor])),
     { provide: API_KEY, useValue: environment?.tmdbApiKey },
     { provide: MOVIE_DB, useValue: new MovieDb(environment?.tmdbApiKey) },
   ]
---
+++ src/core/api.interceptor.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpInterceptor,
+  HttpRequest,
+  HttpHandler,
+  HttpEvent, HttpInterceptorFn, HttpHandlerFn,
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../environments/environment';
+
+
+@Injectable()
+export class ApiInterceptor implements HttpInterceptor {
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+    return apiInterceptor(request, next.handle);
+  }
+}
+
+export const apiInterceptor: HttpInterceptorFn = (req: HttpRequest<unknown>, next: HttpHandlerFn) => {
+  const clonedRequest = req.clone({
+    url: `${ environment.apiUrl }${ req.url }`,
+    setHeaders: {
+      'Content-Type': 'application/json',
+    },
+  });
+
+  return next(clonedRequest);
+};
