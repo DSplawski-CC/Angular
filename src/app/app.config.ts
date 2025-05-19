6a6f71bffa4cecf18a9976ec1d6ab719ced2d00d
Daniel Splawski <daniel.splawski@codeconcept.pl>
2025-05-19 15:39:44 +0200

    cleanup

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,7 +3,6 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { apiInterceptor } from '@core/api.interceptor';
-import { provideImageKitLoader } from '@angular/common';
 import { environment } from '../environments/environment';
 import { ImagekitioAngularModule } from 'imagekitio-angular';
 
@@ -12,7 +11,6 @@ export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    // provideImageKitLoader(environment.imageKit.urlEndpoint),
     ImagekitioAngularModule.forRoot({
       publicKey: '',
       urlEndpoint: environment.imageKit.urlEndpoint,
