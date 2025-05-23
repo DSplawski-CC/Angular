1b5d89e8bb45016c699579ade0e9c0be37e3393f
Daniel Splawski <daniel.splawski@codeconcept.pl>
2025-05-23 08:32:29 +0200

    Implemented spinner component

---
+++ src/shared/components/spinner/spinner.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-spinner',
+  imports: [],
+  templateUrl: './spinner.component.html',
+})
+export class SpinnerComponent {
+}
